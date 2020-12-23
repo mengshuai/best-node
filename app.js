72f51dfdd9d8ab8bdc5f3d3a57259057d5ec9c90
孟帅-商业广告事业部 <mengshuai@zhihu.com>
2020-12-24 00:54:38 +0800

    up

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
 const app = express();
 
 // import 等语法要用到 babel 支持
@@ -14,6 +17,17 @@ var bodyParser = require("body-parser");
 //body parser before routes
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+
+app.use(cookieParser("best_node_cookie"));
+app.use(
+  session({
+    secret: "best_node_cookie",
+    name: "session_id", //# 在浏览器中生成cookie的名称key，默认是connect.sid
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 1000 * 30, httpOnly: true }, //过期时间
+  })
+);
 
 //将路由文件引入
 const route = require("./routes/index");
